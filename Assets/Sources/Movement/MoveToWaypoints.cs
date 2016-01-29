79feb863621ca5e236358b4e469108f839f6b859
Vincent <vincentiu.adam@gmail.com>
2016-01-30 00:32:21 +0100

    Added initial NavMesh baking and waypoint based movement

---
+++ Assets/Sources/Movement/MoveToWaypoints.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToWaypoints : MonoBehaviour 
+{
+	[SerializeField]
+	private Transform[] m_Waypoints;
+
+	[SerializeField]
+	private NavMeshAgent m_NavMeshAgent;
+
+	private int m_CurrentWaypoint;
+
+	private void Start()
+	{
+		//move to first waypoint
+		MoveToWaypoint(1);
+	}
+
+	private void Update()
+	{
+		//if path finished move to next waypoint
+		if (m_NavMeshAgent.remainingDistance == 0)
+		{
+			MoveNext();	
+		}
+	}
+
+	private void MoveNext()
+	{
+		m_CurrentWaypoint = (m_CurrentWaypoint + 1) % m_Waypoints.Length;
+		MoveToWaypoint(m_CurrentWaypoint);
+	}
+
+	private void MoveToWaypoint(int index)
+	{
+		m_NavMeshAgent.destination = m_Waypoints[index].localPosition;
+		m_CurrentWaypoint = index;
+	}
+}
