2f5594154f5c9ed2d4ed98799dd916ef280fb4f4
Louis Gaillard <louisgaillard33@gmail.com>
2016-01-30 20:43:16 +0100

    add timer and end screen

---
+++ Assets/Sources/Timer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    [SerializeField]
+    TextMesh text;
+
+    int minutes = 2, seconds = 0;
+    string minutesStr, secondsStr;
+
+	void Start ()
+    {
+        InvokeRepeating("RemoveOneSecond", 1f, 1f);
+	}
+
+    void RemoveOneSecond()
+    {
+        if (seconds == 0)
+        {
+            seconds = 59;
+            minutes--;
+        }
+        else
+            seconds--;
+
+        if (minutes == 0)
+        {
+            if (seconds == 20)
+                text.color = Color.red;
+            else if (seconds == 0)
+                GetComponent<EndGame>().TimerFinished();
+        }
+
+        if (minutes < 10)
+            minutesStr = "0" + minutes;
+        else
+            minutesStr = minutes.ToString();
+
+        if (seconds < 10)
+            secondsStr = "0" + seconds;
+        else
+            secondsStr = seconds.ToString();
+
+        text.text = minutesStr + ":" + secondsStr;
+    }
+}
