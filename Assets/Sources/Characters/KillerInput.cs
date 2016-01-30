e96fa25258a525848355af530e0d8c5a3db320ef
Vincent <vincentiu.adam@gmail.com>
2016-01-31 00:24:16 +0100

    Added Boost and Z order scripts to killer and NPC

--- Assets/Sources/Characters/KillerInput.cs
+++ Assets/Sources/Characters/KillerInput.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class KillerInput : MonoBehaviour {
+public class KillerInput : MonoBehaviour 
+{
+	[SerializeField]
+	private float boostSpeed;
+
+	[SerializeField]
+	private string boost;
 
 	private Rigidbody rb;
+	private NavMeshAgent navMeshAgent;
+
+	private float speed = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +21,16 @@ public class KillerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * 4f;
+		rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical")).normalized * speed * 4f;
+
+		if (Input.GetButtonDown(boost))
+		{
+			speed = boostSpeed;
+		}
+
+		if (Input.GetButtonUp(boost))
+		{
+			speed = 1f;
+		}
 	}
 }
---
+++ Assets/Sources/SpeedBoost.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBoost : MonoBehaviour 
+{
+	[SerializeField]
+	private float m_Frequency;
+
+	[SerializeField]
+	private float m_Probability;
+
+	[SerializeField]
+	private float m_Boost;
+
+	private NavMeshAgent m_NavMeshAgent;
+
+	//avoids getting boost twice
+	private bool m_HadBoost;
+
+	private float m_DefaultSpeed;
+
+	private void Start()
+	{
+		m_NavMeshAgent = GetComponent<NavMeshAgent>();
+		m_DefaultSpeed = m_NavMeshAgent.speed;
+
+		StartCoroutine(CheckBoost());
+	}
+
+	private IEnumerator CheckBoost()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(m_Frequency);
+
+			float chance = Random.value;
+			if (!m_HadBoost && chance < m_Probability)
+			{
+				m_NavMeshAgent.speed *= m_Boost;
+				m_HadBoost = true;
+			}
+			else
+			{
+				m_HadBoost = false;
+				m_NavMeshAgent.speed = m_DefaultSpeed;
+			}
+		}
+	}
+}
