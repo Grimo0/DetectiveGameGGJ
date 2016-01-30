9e4dd5c46dec4fb0c6e65e62d21b0fbf29e00556
Vincent <vincentiu.adam@gmail.com>
2016-01-30 11:11:53 +0100

    Updated NPCs script
    Updated NPC prefab

---
+++ Assets/Sources/Movement/MoveStop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveStop : NPCBehaviour 
+{
+	[SerializeField]
+	private float m_Delay;
+
+	public override void Move()
+	{
+		//wait at current destination until delay
+		m_NavMeshAgent.destination = transform.position;
+
+		Invoke("Completed", m_Delay);
+	}
+
+	public override void Refresh()
+	{
+		Move();	
+	}
+
+	private void Completed()
+	{
+		OnCompleted.Invoke();
+	}
+}
