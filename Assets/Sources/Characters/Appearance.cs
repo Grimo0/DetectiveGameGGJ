44e73ea0a4004d93a1593439474134484e4f5d1b
Radnap <gregoir.andre@gmail.com>
2016-01-30 11:11:39 +0100

    Prepare for KillerUI integrations

--- Assets/Sources/Characters/Appearance.cs
+++ Assets/Sources/Characters/Appearance.cs
@@ -3,17 +3,34 @@ using System.Collections;
 
 public class Appearance : MonoBehaviour {
 	
-	private CharacterPart hat;
+	private CharacterPart _hat;
 	public SpriteRenderer hatRenderer;
 
-	private CharacterPart head;
+	private CharacterPart _head;
 	public SpriteRenderer headRenderer;
 
-	private CharacterPart body;
+	private CharacterPart _body;
 	public SpriteRenderer bodyRenderer;
 
-	private CharacterPart pant;
+	private CharacterPart _pant;
 	public SpriteRenderer pantRenderer;
+
+
+	public CharacterPart Hat {
+		get { return this._hat; }
+	}
+
+	public CharacterPart Head {
+		get { return this._head; }
+	}
+
+	public CharacterPart Body {
+		get { return this._body; }
+	}
+
+	public CharacterPart Pant {
+		get { return this._pant; }
+	}
 
 
 	// Use this for initialization
@@ -22,16 +39,16 @@ public class Appearance : MonoBehaviour {
 	}
 
 	public void Initialize(CharacterPart hat, CharacterPart head, CharacterPart body, CharacterPart pant) {
-		this.hat = hat;
+		this._hat = hat;
 		this.hatRenderer.sprite = hat.sprite;
 
-		this.head = head;
+		this._head = head;
 		this.headRenderer.sprite = head.sprite;
 
-		this.body = body;
+		this._body = body;
 		this.bodyRenderer.sprite = body.sprite;
 
-		this.pant = pant;
+		this._pant = pant;
 		this.pantRenderer.sprite = pant.sprite;
 	}
 	
--- Assets/Sources/KillerUI.cs
+++ Assets/Sources/KillerUI.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class KillerUI : MonoBehaviour {
 
+	public SpriteRenderer[] mission0;
+	public SpriteRenderer[] mission1;
+	public SpriteRenderer[] mission2;
+	public SpriteRenderer[] mission3;
+	public SpriteRenderer[] mission4;
+
 
 	void Start () {
-	
+	}
+
+	public void Initialize() {
+		Mission[] missions = GetComponent<KillerBehavior>().missions;
 	}
 	
 	void Update () {
